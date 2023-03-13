e7125b28e614bb0aa4b56c637b7411e8c85d17f0
Bryan Navarro <91108786+Dying-Lord@users.noreply.github.com>
2023-03-13 09:20:16 -0700

    Delete Quiz1Project1.py

--- Quiz1Project1.py
+++
@@ -1,9 +0,0 @@
-# Bryan Navarro
-# CMPR114 - Python
-# March 6, 2023
-# Quiz 1 - Project 1
-
-grossSalary = int(input("\nEnter gross salary: "))
-
-adjustedSalary = grossSalary + (grossSalary * .10)
-print(f"\nSalary: ${adjustedSalary:,.2f}")
