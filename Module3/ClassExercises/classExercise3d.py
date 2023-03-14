457b529fa74cc87332103ef887336e26a95feaa4
Bryan Navarro <91108786+Dying-Lord@users.noreply.github.com>
2023-03-13 18:22:20 -0700

    Add files via upload

---
+++ Module3/ClassExercises/classExercise3d.py
@@ -0,0 +1,8 @@
+# Bryan Navarro
+# CMPR114 - Python
+# February 27, 2023
+
+# 2 "For Loops" through two string arrays
+for lastName in ['Navarro']:
+	for firstName in ['Bryan']:
+		print("Your full name is " + firstName + " " + lastName)
---
+++ Module3/ClassExercises/classExercise3e.py
@@ -0,0 +1,7 @@
+# Bryan Navarro
+# CMPR114 - Python
+# February 27, 2023
+
+# Demonstrates the "Range Function" can be used with a for loop
+for x in range(10):
+	print('Hello World')
