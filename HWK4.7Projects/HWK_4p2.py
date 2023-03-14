53029591ac199d0d7ad058f137076e25407bcaf4
Bryan Navarro <91108786+Dying-Lord@users.noreply.github.com>
2023-03-13 18:26:14 -0700

    Delete HWK4.7Projects directory

--- HWK4.7Projects/HWK_4p2.py
+++
@@ -1,8 +0,0 @@
-# Bryan Navarro
-# CMPR114 - Python
-# March 7, 2023
-# HWK_4.7 - Project 2
-
-test_tup = (15, 20, 123, 47, 26, 81)
-max = max(test_tup)
-print(f"\nThe max of the tuple is: {max}")
--- HWK4.7Projects/HWK_4p3.py
+++
@@ -1,9 +0,0 @@
-# Bryan Navarro
-# CMPR114 - Python
-# March 7, 2023
-# HWK_4.7 - Project 3
-
-test_tup = ([17, 28], [93, 11], [20, 17])
-total = sum(test_tup[0] + test_tup[1] + test_tup[2])
-
-print(f"\nTotal: {total}")
