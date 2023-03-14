c15637de15424abae2beb97f572ec91040ae359d
Bryan Navarro <91108786+Dying-Lord@users.noreply.github.com>
2023-03-13 18:25:42 -0700

    Delete ClassExercise3 directory

--- ClassExercise3/CaloriesBurned.py
+++
@@ -1,9 +0,0 @@
-# Bryan Navarro
-# CMPR114 - Python
-# February 27, 2023
-
-caloriesPerMin = 4.2
-
-for minutes in [10,15,20,25,30]:
-	caloriesBurned = caloriesPerMin * minutes
-	print(f"Total calories burned at {minutes} minutes: {caloriesBurned} calories")
--- ClassExercise3/classExercise3d.py
+++
@@ -1,8 +0,0 @@
-# Bryan Navarro
-# CMPR114 - Python
-# February 27, 2023
-
-# 2 "For Loops" through two string arrays
-for lastName in ['Navarro']:
-	for firstName in ['Bryan']:
-		print("Your full name is " + firstName + " " + lastName)
