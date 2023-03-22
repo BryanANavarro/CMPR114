354a0425a50aab4f9901f42bbaf9e88a8037d711
Bryan Navarro <91108786+Dying-Lord@users.noreply.github.com>
2023-03-22 13:23:09 -0700

    Add files via upload

--- Module6/ClassExercise/classExercise5c.py
+++ Module6/ClassExercise/classExercise5c.py
@@ -7,18 +7,20 @@ def sales():
 	
 	salary = float(input("\nEnter salary: "))
 	num_days = int(input("\nEnter the days of sales: "))
-	sales_file = open('sales.txt', 'a')
+	sales_file = open('sales.txt', 'w')
 	sum = 0.0
 	for count in range(1, num_days + 1):
 
 		sales = float(input("Enter the sales for day # " + str(count) + " : "))
-		sales_file.write(str(sales) + '\n')
+		sales_file.write("Sale Day # " + str(count) + " : " + str(sales) + '\n')
 		sum += sales
 
 	
 	if sum > 1000:
-			salary = salary + (salary* .10)
-			sales_file.write(str(salary) + '\n')
+		salary = salary + (salary* .10)
+		sales_file.write("Salary plus bonus : " + str(salary) + '\n')
+	else:
+		sales_file.write("Salary : " + str(salary))
 	sales_file.close()
 	print("\nSales recorded.")
 sales()
@@ -29,9 +31,9 @@ def ReadSales():
 	line = sales_file.readline()
 
 	while line != '': 
-		amount = float(line)
-
-		print(f"{amount:.2f}")
+		amount = str(line)
+		
+		print(f"{amount}")
 		line = sales_file.readline()
 	sales_file.close()
 ReadSales()
